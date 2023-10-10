5f61f0038a54017345f35fa97025810c31c8a54e
i-be-snek <sjzahra@proton.me>
2023-10-10 16:52:26 +0200

    :sparkles: Add scheduling

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ from tinydb import TinyDB
 from src.auth import lemmy_auth, reddit_oauth
 from src.helper import Config
 from src.mirror import get_threads_from_reddit, mirror_threads_to_lemmy
+import datetime
 
 
-def mirror(db_path: str = "db.json"):
+def mirror(db_path: str = "db.json", limit: int = 10):
+    print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
     config = Config(dotenv_values(".env"))
 
     reddit = reddit_oauth(config)
@@ -16,7 +18,7 @@ def mirror(db_path: str = "db.json"):
     if not reddit:
         return
 
-    threads = get_threads_from_reddit(reddit, config.REDDIT_SUBREDDIT, DB, limit=10)
+    threads = get_threads_from_reddit(reddit, config.REDDIT_SUBREDDIT, DB, limit=limit)
 
     if threads:
         lemmy = lemmy_auth(config)
@@ -30,4 +32,10 @@ def mirror(db_path: str = "db.json"):
 
 
 if __name__ == "__main__":
-    mirror()
+    import schedule
+
+    print(f"Scheduler started at {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+    schedule.every(30).seconds.do(mirror, limit=10)
+
+    while True:
+        schedule.run_pending()
