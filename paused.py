2524d5edc502e66cb61fcaae2e9767b89cbcd7a9
i-be-snek <sjzahra@proton.me>
2023-10-27 15:05:06 +0200

    :rocket: Deploy 'paused' script

---
+++ paused.py
@@ -0,0 +1,12 @@
+import time
+import logging
+
+logging.basicConfig(
+    format="%(asctime)s %(levelname)-8s %(message)s",
+    level=logging.INFO,
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+
+while True:
+    logging.warn("Worker paused")
+    time.sleep(120)
