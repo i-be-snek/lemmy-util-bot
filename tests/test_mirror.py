2e1483a5cfc7c79512ccc0b85c054854b2ecffad
i-be-snek <sjzahra@proton.me>
2023-10-27 15:07:30 +0200

    :white_check_mark: Add tests for listing and ignore filters

--- tests/test_mirror.py
+++ tests/test_mirror.py
@@ -5,15 +5,10 @@ import praw
 import pytest
 from tinydb import Query, TinyDB
 
-from src.helper import Config
-from src.mirror import (
-    _check_thread_in_db,
-    _extract_threads_to_mirror,
-    _getattr_mod,
-    _insert_thread_into_db,
-    get_threads_from_reddit,
-    mirror_threads_to_lemmy,
-)
+from src.helper import Config, Thread
+from src.mirror import (_check_thread_in_db, _extract_threads_to_mirror,
+                        _getattr_mod, _insert_thread_into_db,
+                        get_threads_from_reddit, mirror_threads_to_lemmy)
 from tests import items
 
 
@@ -31,11 +26,45 @@ class TestClassMirror:
         assert _extract_threads_to_mirror([], test_db) == list(dict())
         test_db.close()
 
-    def test_get_threads_from_reddit(self):
-        # reddit = None
-        # threads = get_threads_from_reddit(reddit, "test_sub", test_db)
-        # assert threads == []
-        pass
+    def test__extract_threads_to_mirror_ignore_poll_no_valid_threads(self):
+        test_db = TinyDB(items.test_db_path)
+        mock_listing = mock.MagicMock()
+        mock_listing.name = "tg_2ggk3"
+        mock_listing.stickied = None
+        mock_listing.over_18 = False
+        mock_listing.poll_data = True  # is a poll thread
+        mock_listing.is_locked = False
+        mock_listing.is_video = False
+        mock_listing.url = "https://example.com"
+        mock_listing.title = "Test title"
+        mock_listing.selftext = "Test body"
+        mock_listing.permalink = "comments/tg_2ggk3"
+        mock_listing.link_flair_text = None
+
+        threads_to_mirror = _extract_threads_to_mirror(
+            [mock_listing], test_db, [Thread.poll]
+        )
+        assert len(threads_to_mirror) == 0
+
+    def test__extract_threads_to_mirror_ignore_poll_two_valid_threads(self):
+        test_db = TinyDB(items.test_db_path)
+        mock_listing = mock.MagicMock()
+        mock_listing.name = "tg_2ggk3"
+        mock_listing.stickied = None
+        mock_listing.over_18 = False
+        mock_listing.poll_data = False  # is not a poll thread
+        mock_listing.is_locked = False
+        mock_listing.is_video = False
+        mock_listing.url = "https://example.com"
+        mock_listing.title = "Test title"
+        mock_listing.selftext = "Test body"
+        mock_listing.permalink = "comments/tg_2ggk3"
+        mock_listing.link_flair_text = None
+
+        threads_to_mirror = _extract_threads_to_mirror(
+            [mock_listing, mock_listing], test_db, [Thread.poll]
+        )
+        assert len(threads_to_mirror) == 2
 
     def test_mirror_threads_to_lemmy_no_threads(self):
         test_db = TinyDB(items.test_db_path)
