346c4342f4a7dfb564a08802699c9c69c15142b8
i-be-snek <sjzahra@proton.me>
2023-10-31 08:14:19 +0100

    :construction: Work in progress

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import os
 from src.auth import lemmy_auth, reddit_oauth
 from src.helper import Config, DataBase
 from src.mirror import get_threads_from_reddit, mirror_threads_to_lemmy
+from src.auto_mod import AutoMod
 import praw
 import logging
 
@@ -65,56 +66,59 @@ if __name__ == "__main__":
     filter_limit = config.REDDIT_FILTER_THREAD_LIMIT
 
     # get latest backup of the database
-    database_path = "data/mirrored_threads.json"
-    filestack = DataBase(db_path=database_path)
-    filestack.get_backup(
-        app_secret=config.FILESTACK_APP_SECRET,
-        apikey=config.FILESTACK_API_KEY,
-        handle=config.FILESTACK_HANDLE_REFRESH,
-    )
+    # database_path = "data/mirrored_threads.json"
+    # filestack = DataBase(db_path=database_path)
+    # filestack.get_backup(
+    #     app_secret=config.FILESTACK_APP_SECRET,
+    #     apikey=config.FILESTACK_API_KEY,
+    #     handle=config.FILESTACK_HANDLE_REFRESH,
+    # )
 
     def raiseError(e):
         raise e
 
     # confirm the file has been downloaded
-    assert os.path.exists(database_path) or raiseError(FileNotFoundError)
+    # assert os.path.exists(database_path) or raiseError(FileNotFoundError)
 
     # initialize database
-    database = TinyDB(database_path)
+    # database = TinyDB(database_path)
 
     # authenticate with reddit once at the beginning
-    reddit = reddit_oauth(config)
+    # reddit = reddit_oauth(config)
 
     # if threads exist, authenticate with lemmy and mirror threads
-    schedule.every(mirror_s).seconds.do(
-        mirror,
-        reddit=reddit,
-        database=database,
-        filter=config.FILTER_BY,
-        limit=filter_limit,
-        mirror_delay=mirror_delay_s,
-    )
-    logging.info(f"Scheduler started")
-    logging.info(f"Mirroring every {mirror_s} seconds with a delay of {mirror_delay_s} seconds between threads; checking up to {filter_limit} threads at a time")
-    logging.info(f"Refreshing the database file every {refresh_m} minutes; creating a backup copy every {backup_h} hours")
 
-    # refresh the database file in filestack
-    schedule.every(refresh_m).minutes.do(
-        filestack.refresh_backup,
-        app_secret=config.FILESTACK_APP_SECRET,
-        apikey=config.FILESTACK_API_KEY,
-        handle=config.FILESTACK_HANDLE_REFRESH,
-    )
+    schedule.every(30).seconds.do(automod_comment_on_new_threads, config=config)
 
-    # refresh the database backup in filestack
-    schedule.every(backup_h).hours.do(
-        filestack.refresh_backup,
-        app_secret=config.FILESTACK_APP_SECRET,
-        apikey=config.FILESTACK_API_KEY,
-        handle=config.FILESTACK_HANDLE_BACKUP,
-    )
+    # schedule.every(mirror_s).seconds.do(
+    #     mirror,
+    #     reddit=reddit,
+    #     database=database,
+    #     filter=config.FILTER_BY,
+    #     limit=filter_limit,
+    #     mirror_delay=mirror_delay_s,
+    # )
+    # logging.info(f"Scheduler started")
+    # logging.info(f"Mirroring every {mirror_s} seconds with a delay of {mirror_delay_s} seconds between threads; checking up to {filter_limit} threads at a time")
+    # logging.info(f"Refreshing the database file every {refresh_m} minutes; creating a backup copy every {backup_h} hours")
+    #  
+    # # refresh the database file in filestack
+    # schedule.every(refresh_m).minutes.do(
+    #     filestack.refresh_backup,
+    #     app_secret=config.FILESTACK_APP_SECRET,
+    #     apikey=config.FILESTACK_API_KEY,
+    #     handle=config.FILESTACK_HANDLE_REFRESH,
+    # )
+    #
+    # # refresh the database backup in filestack
+    # schedule.every(backup_h).hours.do(
+    #     filestack.refresh_backup,
+    #     app_secret=config.FILESTACK_APP_SECRET,
+    #     apikey=config.FILESTACK_API_KEY,
+    #     handle=config.FILESTACK_HANDLE_BACKUP,
+    # )
 
     # start scheduler
-    with database:
-        while True:
-            schedule.run_pending()
+    #with database:
+    while True:
+        schedule.run_pending()
