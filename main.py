68b97718bdcc178704933ae9505056f75a536d4d
i-be-snek <sjzahra@proton.me>
2023-11-05 16:51:36 +0100

    :bricks: Add mod bot util tasks to schedule

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ import schedule
 import os
 
 from src.auth import lemmy_auth, reddit_oauth
-from src.helper import Config, DataBase
+from src.helper import Config, DataBase, Task
 from src.mirror import get_threads_from_reddit, mirror_threads_to_lemmy
 from src.auto_mod import AutoMod
 import praw
 import logging
+from pythorhead import Lemmy
 
 logging.basicConfig(
     format="%(asctime)s %(levelname)-8s %(message)s",
@@ -49,76 +50,96 @@ def mirror(
         )
 
 
-def automod_comment_on_new_threads(config: dict):
-    lemmy = lemmy_auth(config)
+def automod_comment_on_new_threads(config: dict, lemmy: Lemmy):
     auto_mod = AutoMod(lemmy, config.LEMMY_COMMUNITY, config.LEMMY_USERNAME)
     auto_mod.comment_on_new_threads()
+
+def raiseError(e):
+    raise e
 
 
 if __name__ == "__main__":
     # get config
     config = Config(dotenv_values(".env"))
+    needs_database: tuple = (Task.mirror_threads)
 
-    backup_h = config.BACKUP_FILESTACK_EVERY_HOUR
-    refresh_m = config.REFRESH_FILESTACK_EVERY_MINUTE
-    mirror_s = config.MIRROR_THREADS_EVERY_SECOND
-    mirror_delay_s = config.DELAY_BETWEEN_MIRRORED_THREADS_SECOND
-    filter_limit = config.REDDIT_FILTER_THREAD_LIMIT
+    # authenticate with lemmy
+    lemmy = lemmy_auth(config)
 
-    # get latest backup of the database
-    # database_path = "data/mirrored_threads.json"
-    # filestack = DataBase(db_path=database_path)
-    # filestack.get_backup(
-    #     app_secret=config.FILESTACK_APP_SECRET,
-    #     apikey=config.FILESTACK_API_KEY,
-    #     handle=config.FILESTACK_HANDLE_REFRESH,
-    # )
+    # schedule tasks
+    if Task.comment_on_new_threads in config.TASKS:
+        interval = 120
+        schedule.every(interval).seconds.do(automod_comment_on_new_threads, config=config, lemmy=lemmy)
+        logging.info(f"Checking for new posts every {interval} seconds")
 
-    def raiseError(e):
-        raise e
+    if Task.mirror_threads in config.TASKS:
 
-    # confirm the file has been downloaded
-    # assert os.path.exists(database_path) or raiseError(FileNotFoundError)
+        backup_h = config.BACKUP_FILESTACK_EVERY_HOUR
+        refresh_m = config.REFRESH_FILESTACK_EVERY_MINUTE
+        mirror_s = config.MIRROR_THREADS_EVERY_SECOND
+        mirror_delay_s = config.DELAY_BETWEEN_MIRRORED_THREADS_SECOND
+        filter_limit = config.REDDIT_FILTER_THREAD_LIMIT
 
-    # initialize database
-    # database = TinyDB(database_path)
+        # get latest backup of the database
+        database_path = "data/mirrored_threads.json"
+        filestack = DataBase(db_path=database_path)
+        filestack.get_backup(
+            app_secret=config.FILESTACK_APP_SECRET,
+            apikey=config.FILESTACK_API_KEY,
+            handle=config.FILESTACK_HANDLE_REFRESH,
+        )
 
-    # authenticate with reddit once at the beginning
-    # reddit = reddit_oauth(config)
 
-    # if threads exist, authenticate with lemmy and mirror threads
+        # confirm the file has been downloaded
+        assert os.path.exists(database_path) or raiseError(FileNotFoundError)
 
-    schedule.every(30).seconds.do(automod_comment_on_new_threads, config=config)
+        # initialize database
+        database = TinyDB(database_path)
 
-    # schedule.every(mirror_s).seconds.do(
-    #     mirror,
-    #     reddit=reddit,
-    #     database=database,
-    #     filter=config.FILTER_BY,
-    #     limit=filter_limit,
-    #     mirror_delay=mirror_delay_s,
-    # )
-    # logging.info(f"Scheduler started")
-    # logging.info(f"Mirroring every {mirror_s} seconds with a delay of {mirror_delay_s} seconds between threads; checking up to {filter_limit} threads at a time")
-    # logging.info(f"Refreshing the database file every {refresh_m} minutes; creating a backup copy every {backup_h} hours")
-    #  
-    # # refresh the database file in filestack
-    # schedule.every(refresh_m).minutes.do(
-    #     filestack.refresh_backup,
-    #     app_secret=config.FILESTACK_APP_SECRET,
-    #     apikey=config.FILESTACK_API_KEY,
-    #     handle=config.FILESTACK_HANDLE_REFRESH,
-    # )
-    #
-    # # refresh the database backup in filestack
-    # schedule.every(backup_h).hours.do(
-    #     filestack.refresh_backup,
-    #     app_secret=config.FILESTACK_APP_SECRET,
-    #     apikey=config.FILESTACK_API_KEY,
-    #     handle=config.FILESTACK_HANDLE_BACKUP,
-    # )
+        # authenticate with reddit once at the beginning
+        reddit = reddit_oauth(config)
 
-    # start scheduler
-    #with database:
-    while True:
-        schedule.run_pending()
+        # if threads exist, authenticate with lemmy and mirror threads
+
+
+        schedule.every(mirror_s).seconds.do(
+            mirror,
+            reddit=reddit,
+            database=database,
+            filter=config.FILTER_BY,
+            limit=filter_limit,
+            mirror_delay=mirror_delay_s,
+        )
+        # refresh the database file in filestack
+        schedule.every(refresh_m).minutes.do(
+            filestack.refresh_backup,
+            app_secret=config.FILESTACK_APP_SECRET,
+            apikey=config.FILESTACK_API_KEY,
+            handle=config.FILESTACK_HANDLE_REFRESH,
+        )
+
+        # refresh the database backup in filestack
+        schedule.every(backup_h).hours.do(
+            filestack.refresh_backup,
+            app_secret=config.FILESTACK_APP_SECRET,
+            apikey=config.FILESTACK_API_KEY,
+            handle=config.FILESTACK_HANDLE_BACKUP,
+        )
+
+        logging.info(f"Mirroring every {mirror_s} seconds with a delay of {mirror_delay_s} seconds between threads; checking up to {filter_limit} threads at a time")
+        logging.info(f"Refreshing the database file every {refresh_m} minutes; creating a backup copy every {backup_h} hours")
+
+
+
+    if any([x in needs_database for x in config.TASKS]):
+        # start scheduler with database
+        logging.info(f"Scheduler started")
+        with database:
+            while True:
+                schedule.run_pending()
+
+    else:
+        # otherwise, run without database
+        logging.info(f"Scheduler started")
+        while True:
+            schedule.run_pending()
